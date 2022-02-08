75e38de3a340dae1079e48209e9c0680c1b3a9f8
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-02-08 18:27:17 -0300

    Iniciada a estilização do título e dos links do Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import earphoneImg from '../images/earphone.svg';
 
 class Header extends React.Component {
   constructor() {
@@ -26,17 +27,29 @@ class Header extends React.Component {
     const { userName, loading } = this.state;
     return (
       <header data-testid="header-component">
-        <h1>TrybeTunes</h1>
-        <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
-        <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
-        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
-        {loading
-          ? <Loading />
-          : (
-            <h2 data-testid="header-user-name">
-              {userName}
-            </h2>
-          )}
+        <div className="title-container">
+          <h1>trybe</h1>
+          <img
+            className="earphone-img"
+            src={ earphoneImg }
+            alt="Fones de ouvido"
+          />
+          <h1>tunes</h1>
+        </div>
+        <div className="user-container">
+          {loading
+            ? <Loading />
+            : (
+              <h2 data-testid="header-user-name">
+                {userName}
+              </h2>
+            )}
+        </div>
+        <div className="links-container">
+          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </div>
       </header>
     );
   }
